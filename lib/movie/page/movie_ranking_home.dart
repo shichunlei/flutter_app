de8dec1c40274d06913d25a6356424f63ca92708
师春雷 <shichunlei@14cells.com>
2019-03-01 16:55:34 +0800

    添加电影排行榜

---
+++ lib/movie/page/movie_ranking_home.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/movie/page/sub_page/new_movie_raking.dart';
+import 'package:flutter_app/movie/page/sub_page/us_movie_raking.dart';
+import 'package:flutter_app/movie/page/sub_page/weekly_movie_raking.dart';
+import 'package:flutter_app/utils/api.dart';
+
+class MovieRakingHome extends StatefulWidget {
+  @override
+  _MovieRakingHomeState createState() => _MovieRakingHomeState();
+}
+
+class _MovieRakingHomeState extends State<MovieRakingHome>
+    with SingleTickerProviderStateMixin {
+  TabController controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Initialize the Tab Controller
+    controller = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispost() {
+    // Dispost of the Tab Controller
+    controller.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('电影排行榜'),
+        backgroundColor: Colors.purple,
+        bottom: _tabBar(),
+      ),
+      body: _tabBarView(<Widget>[
+        NewMovieRakingPage(),
+        WeeklyMovieRakingPage(),
+        UsMovieRakingPage(),
+      ]),
+    );
+  }
+
+  TabBar _tabBar() {
+    return TabBar(
+      isScrollable: true,
+      tabs: <Tab>[
+        Tab(text: "新片榜"),
+        Tab(text: "口碑榜"),
+        Tab(text: "北美票房榜"),
+      ],
+      // setup the controller
+      controller: controller,
+    );
+  }
+
+  TabBarView _tabBarView(var tabs) {
+    return TabBarView(
+      // add tabs as widgets
+      children: tabs,
+      // set the controller
+      controller: controller,
+    );
+  }
+}
--- lib/utils/api.dart
+++ lib/utils/api.dart
@@ -3,4 +3,10 @@ class Api {
   static final String MOVIE_LIST_URL = "/in_theaters";
   static final String MOVIE_DETAIL_URL = "/subject";
   static final String MOVIE_TOP250_URL = "/top250";
+
+  static final String NEW_MOVIES_URL = "/new_movies";
+
+  static final String WEEKLY_MOVIES_URL = "/weekly";
+
+  static final String US_MOVIES_URL = "/us_box";
 }
