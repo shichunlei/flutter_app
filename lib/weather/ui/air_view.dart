a471199f778da056b39bf7389ba3505a07c12a57
师春雷 <shichunlei@14cells.com>
2019-05-09 17:44:31 +0800

    添加空气质量指数半圆形进度条

--- lib/weather/ui/air_view.dart
+++ lib/weather/ui/air_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/bean/air_now_city.dart';
+import 'package:flutter_app/custom_widgets/seml_circle_progress.dart';
 import 'package:flutter_app/global/custom_icon.dart';
 import 'package:flutter_app/weather/ui/line.dart';
 
@@ -11,70 +12,82 @@ class AirView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        margin: EdgeInsets.only(top: 10.0),
-        color: Color(0x2a000000),
-        padding: EdgeInsets.all(10.0),
-        child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text('空气质量指数', style: TextStyle(color: Colors.white)),
-              Line(),
-              Row(children: <Widget>[
-                Expanded(
-                  child: Row(children: <Widget>[
-                    Icon(CustomIcon.pm25, color: Colors.white, size: 20.0),
-                    SizedBox(width: 5.0),
-                    Text('  ${air_now_city?.pm25} μg/m3',
-                        style: TextStyle(color: Colors.white))
-                  ]),
-                ),
-                Expanded(
-                  child: Row(children: <Widget>[
-                    Icon(CustomIcon.pm25, color: Colors.white, size: 20.0),
-                    SizedBox(width: 5.0),
-                    Text('  ${air_now_city?.pm10} μg/m3',
-                        style: TextStyle(color: Colors.white))
-                  ]),
-                )
-              ]),
-              Line(),
-              Row(children: <Widget>[
-                Expanded(
-                  child: Row(children: <Widget>[
-                    Icon(CustomIcon.o3, color: Colors.white, size: 20.0),
-                    SizedBox(width: 5.0),
-                    Text('  ${air_now_city?.o3} μg/m3',
-                        style: TextStyle(color: Colors.white))
-                  ]),
-                ),
-                Expanded(
-                  child: Row(children: <Widget>[
-                    Icon(CustomIcon.so2, color: Colors.white, size: 20.0),
-                    SizedBox(width: 5.0),
-                    Text('  ${air_now_city?.so2} μg/m3',
-                        style: TextStyle(color: Colors.white))
-                  ]),
-                )
-              ]),
-              Line(),
-              Row(children: <Widget>[
-                Expanded(
-                  child: Row(children: <Widget>[
+      margin: EdgeInsets.only(top: 10.0),
+      color: Color(0x2a000000),
+      padding: EdgeInsets.all(10.0),
+      child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text('空气质量指数', style: TextStyle(color: Colors.white)),
+            Line(),
+            Container(
+                alignment: Alignment.center,
+                height: 150,
+                child: SemlCircleProgress(
+                    progress: double.parse(air_now_city.aqi) / 500,
+                    strokeWidth: 15.0,
+                    lableFontSize: 40.0,
+                    progressColor: air_now_city.aqiColor,
+                    lable: '${air_now_city.aqi}')),
+            Row(children: <Widget>[
+              Expanded(
+                child: Row(children: <Widget>[
+                  Icon(CustomIcon.pm25, color: Colors.white, size: 20.0),
+                  SizedBox(width: 5.0),
+                  Text('  ${air_now_city?.pm25} μg/m3',
+                      style: TextStyle(color: Colors.white))
+                ]),
+              ),
+              Expanded(
+                child: Row(children: <Widget>[
+                  Icon(CustomIcon.pm25, color: Colors.white, size: 20.0),
+                  SizedBox(width: 5.0),
+                  Text('  ${air_now_city?.pm10} μg/m3',
+                      style: TextStyle(color: Colors.white))
+                ]),
+              )
+            ]),
+            Line(),
+            Row(children: <Widget>[
+              Expanded(
+                child: Row(children: <Widget>[
+                  Icon(CustomIcon.o3, color: Colors.white, size: 20.0),
+                  SizedBox(width: 5.0),
+                  Text('  ${air_now_city?.o3} μg/m3',
+                      style: TextStyle(color: Colors.white))
+                ]),
+              ),
+              Expanded(
+                child: Row(children: <Widget>[
+                  Icon(CustomIcon.so2, color: Colors.white, size: 20.0),
+                  SizedBox(width: 5.0),
+                  Text('  ${air_now_city?.so2} μg/m3',
+                      style: TextStyle(color: Colors.white))
+                ]),
+              )
+            ]),
+            Line(),
+            Row(children: <Widget>[
+              Expanded(
+                child: Row(
+                  children: <Widget>[
                     Icon(CustomIcon.co, color: Colors.white, size: 20.0),
                     SizedBox(width: 5.0),
                     Text('  ${air_now_city?.co} mg/m3',
                         style: TextStyle(color: Colors.white))
-                  ]),
+                  ],
                 ),
-                Expanded(
-                  child: Row(children: <Widget>[
-                    Icon(CustomIcon.no2, color: Colors.white, size: 20.0),
-                    SizedBox(width: 5.0),
-                    Text('  ${air_now_city?.no2} μg/m3',
-                        style: TextStyle(color: Colors.white))
-                  ]),
-                )
-              ])
-            ]));
+              ),
+              Expanded(
+                child: Row(children: <Widget>[
+                  Icon(CustomIcon.no2, color: Colors.white, size: 20.0),
+                  SizedBox(width: 5.0),
+                  Text('  ${air_now_city?.no2} μg/m3',
+                      style: TextStyle(color: Colors.white))
+                ]),
+              )
+            ]),
+          ]),
+    );
   }
 }
--- lib/weather/ui/lifestyle_view.dart
+++ lib/weather/ui/lifestyle_view.dart
@@ -13,36 +13,36 @@ class LifestyleView extends StatelessWidget {
       margin: EdgeInsets.only(top: 10.0),
       color: Color(0x2a000000),
       padding: EdgeInsets.all(10.0),
-      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: <
-          Widget>[
-        Text('生活指数', style: TextStyle(color: Colors.white)),
-        Line(),
-        ListView.builder(
-            itemBuilder: (context, index) => Card(
-                margin: EdgeInsets.symmetric(horizontal: 0.0, vertical: 4.0),
-                color: Color(0x02000000),
-                child: Container(
-                  padding: EdgeInsets.all(5.0),
-                  child: Column(children: <Widget>[
-                    Row(children: <Widget>[
-                      Text(lifestyle[index].type,
-                          style: TextStyle(color: Colors.white)),
-                      Text(lifestyle[index].brf,
-                          style: TextStyle(color: Colors.white))
-                    ], mainAxisAlignment: MainAxisAlignment.spaceBetween),
-                    Container(
-                        height: 0.2,
-                        color: Colors.white,
-                        margin: EdgeInsets.symmetric(vertical: 5.0)),
-                    Text(lifestyle[index].txt,
-                        style: TextStyle(color: Colors.white))
-                  ]),
-                )),
-            itemCount: lifestyle.length,
-            physics: NeverScrollableScrollPhysics(),
-            shrinkWrap: true,
-            primary: false)
-      ]),
+      child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text('生活指数', style: TextStyle(color: Colors.white)),
+            Line(),
+            ListView.builder(
+                itemBuilder: (context, index) => Card(
+                    color: Color(0x02000000),
+                    child: Container(
+                      padding: EdgeInsets.all(5.0),
+                      child: Column(children: <Widget>[
+                        Row(children: <Widget>[
+                          Text(lifestyle[index].type,
+                              style: TextStyle(color: Colors.white)),
+                          Text(lifestyle[index].brf,
+                              style: TextStyle(color: Colors.white))
+                        ], mainAxisAlignment: MainAxisAlignment.spaceBetween),
+                        Container(
+                            height: 0.2,
+                            color: Colors.white,
+                            margin: EdgeInsets.symmetric(vertical: 5.0)),
+                        Text(lifestyle[index].txt,
+                            style: TextStyle(color: Colors.white))
+                      ]),
+                    )),
+                itemCount: lifestyle.length,
+                physics: NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                primary: false)
+          ]),
     );
   }
 }
