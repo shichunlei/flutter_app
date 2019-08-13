f0d8e5b7b3f7034b7847a305d858c774ecb9d05d
师春雷 <shichunlei@14cells.com>
2019-08-13 11:12:13 +0800

    fix [#12]

--- lib/main.dart
+++ lib/main.dart
@@ -25,8 +25,9 @@ void main() async {
 
       if (Platform.isAndroid) {
         // 以下两行 设置android状态栏为透明的沉浸。写在组件渲染之后，是为了在渲染后进行set赋值，覆盖状态栏，写在渲染之前MaterialApp组件会覆盖掉这个值。
-        // SystemUiOverlayStyle systemUiOverlayStyle = SystemUiOverlayStyle(statusBarColor: Colors.transparent);
-        // SystemChrome.setSystemUIOverlayStyle(systemUiOverlayStyle);
+        SystemUiOverlayStyle systemUiOverlayStyle =
+            SystemUiOverlayStyle(statusBarColor: Colors.transparent);
+        SystemChrome.setSystemUIOverlayStyle(systemUiOverlayStyle);
       }
     });
   }, onError: (Object error, StackTrace stack) {
