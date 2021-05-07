87970facc1f07930710c59d04628fff7496f1862
师春雷 <1558053958@qq.com>
2021-05-07 12:28:38 +0800

    修改

--- flutter_libs/lib/flutter_libs.dart
+++ flutter_libs/lib/flutter_libs.dart
@@ -31,7 +31,6 @@ export 'sparkline.dart';
 export 'stick_widget.dart';
 export 'titled_navigation_bar.dart';
 export 'utils.dart';
-export 'wave.dart';
 
 export 'dialog/asset_giffy_dialog.dart';
 export 'dialog/network_giffy_dialog.dart';
